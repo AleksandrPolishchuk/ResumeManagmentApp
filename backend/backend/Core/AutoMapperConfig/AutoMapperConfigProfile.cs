6fc442a3adb84e11397c9d9c086d437c7f7ef5b9
AP <aleksandrpolishchuk@gmail.com>
2023-05-09 23:53:03 +0700

    bug fix

---
+++ backend/backend/Core/AutoMapperConfig/AutoMapperConfigProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using backend.Core.Dtos.Company;
+using backend.Core.Dtos.Job;
+using backend.Core.Entities;
+
+namespace backend.Core.AutoMapperConfig
+{
+	public class AutoMapperConfigProfile: Profile
+	{
+		public AutoMapperConfigProfile()
+		{
+			// Company
+			CreateMap<CompanyCreateDto, Company>();
+			CreateMap<Company, CompanyGetDto>();
+
+			// Job
+			CreateMap<JobCreateDto, Job>();
+			CreateMap<Job, JobGetDto>()
+				.ForMember(dest => dest.CompanyName, opt => opt.MapFrom(src => src.Company.Name));
+
+			// Candidate
+		}
+	}
+}
---
+++ backend/backend/Core/Dtos/Job/JobGetDto.cs
@@ -0,0 +1,14 @@
+﻿using backend.Core.Enums;
+
+namespace backend.Core.Dtos.Job
+{
+	public class JobGetDto
+	{
+		public long ID { get; set; }
+		public string Title { get; set; }
+		public JobLevel Level { get; set; }
+		public long CompanyId { get; set; }
+		public string CompanyName { get; set; }
+		public DateTime CreatedAt { get; set; } = DateTime.Now;
+	}
+}
