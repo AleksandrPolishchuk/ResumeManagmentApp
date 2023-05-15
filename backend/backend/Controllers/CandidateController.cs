3c74fbdf3d55c72e7cdd5d704a98c9dd41694811
AP <aleksandrpolishchuk@gmail.com>
2023-05-15 23:44:51 +0700

    add documents/pdf folder

--- backend/backend/Controllers/CandidateController.cs
+++ backend/backend/Controllers/CandidateController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using backend.Core.Context;
+using backend.Core.Dtos.Candidate;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -21,6 +22,23 @@ namespace backend.Controllers
 		// CRUD 
 
 		// Create
+		[HttpPost]
+		[Route("Create")]
+		public async Task<IActionResult> CreateCandidate([FromForm] CandidateCreateDto dto, IFormFile pdfFile)
+		{
+			// Firt => Save pdf to Server
+			// Then => save url into our entity
+			var fiveMegaByte = 5 * 1024 * 1024;
+			var pdfMimeType = "application/json";
+
+			if (pdfFile.Length > fiveMegaByte || pdfFile.ContentType != pdfMimeType)
+			{
+				return BadRequest("File is not valid");
+			}
+
+			var resumeUrl = Guid.NewGuid().ToString() + ".pdf";
+			var filePath = Path.Combine(Directory.GetCurrentDirectory(), "documents", "pdfs", resumeUrl);
+		}
 
 		// Read
 
