a7c747e634ceb317012d486c3626a912e22d9412
AP <aleksandrpolishchuk@gmail.com>
2023-05-15 23:33:03 +0700

    add CandidateController

---
+++ backend/backend/Controllers/CandidateController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using backend.Core.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CandidateController : ControllerBase
+	{
+		private ApplicationDbContext _context { get; }
+		private IMapper _mapper { get; }
+
+		public CandidateController(ApplicationDbContext context, IMapper mapper)
+		{
+			_context = context;
+			_mapper = mapper;
+		}
+
+		// CRUD 
+
+		// Create
+
+		// Read
+
+		// Read (Get Job By ID)
+
+		// Update
+
+		// Delete
+	}
+}
