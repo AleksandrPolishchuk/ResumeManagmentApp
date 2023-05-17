81a33ba365cbc14b0fa64d7a9663619320dcd1cc
AP <aleksandrpolishchuk@gmail.com>
2023-05-17 23:29:53 +0700

    add navbar logic

--- frontend/src/components/navbar/Navbar.component.tsx
+++ frontend/src/components/navbar/Navbar.component.tsx
@@ -1,9 +1,30 @@
-import "./navbsr.scss";
+import "./navbar.scss";
+import { Link } from "react-router-dom";
+
+const links = [
+  { href: "/", label: "Home" },
+  { href: "/companies", label: "Companies" },
+  { href: "/jobs", label: "Jobs" },
+  { href: "/candidates", label: "Candidates" },
+];
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      <div className="brand"></div>
+      <div className="brand">
+        <span>Resume Managment</span>
+      </div>
+      <div className="menu">
+        <ul>
+          {links.map((item) => (
+            <li>
+              <Link to={item.href}>{item.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="hamburger"></div>
+      <div className="toggle"></div>
     </div>
   );
 };
