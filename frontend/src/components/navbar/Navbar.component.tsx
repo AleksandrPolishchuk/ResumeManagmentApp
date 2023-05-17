949ab42084f42f32503af871fd3db8fed9752307
AP <aleksandrpolishchuk@gmail.com>
2023-05-17 23:45:28 +0700

    bug fix

--- frontend/src/components/navbar/Navbar.component.tsx
+++ frontend/src/components/navbar/Navbar.component.tsx
@@ -1,7 +1,9 @@
+import { useContext } from "react";
 import "./navbar.scss";
 import { Link } from "react-router-dom";
-import { Menu } from "@mui/icons-material";
+import { Menu, LightMode, DarkMode } from "@mui/icons-material";
 import { ToggleButton } from "@mui/material";
+import { ThemeContext } from "../../context/theme.context";
 
 const links = [
   { href: "/", label: "Home" },
@@ -11,6 +13,7 @@ const links = [
 ];
 
 const Navbar = () => {
+  const { darkMode, toggleDarkMode } = useContext(ThemeContext);
   return (
     <div className="navbar">
       <div className="brand">
@@ -29,7 +32,13 @@ const Navbar = () => {
         <Menu />
       </div>
       <div className="toggle">
-        <ToggleButton value={"check"}></ToggleButton>
+        <ToggleButton
+          value={"check"}
+          selected={darkMode}
+          onChange={toggleDarkMode}
+        >
+          {darkMode ? <LightMode /> : <DarkMode />}
+        </ToggleButton>
       </div>
     </div>
   );
