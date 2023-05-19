8658aac89681f9a12eff81c9c6e4602da33be2c7
AP <aleksandrpolishchuk@gmail.com>
2023-05-19 23:50:54 +0700

    add homepage

---
+++ frontend/src/pages/home/Home.page.tsx
@@ -0,0 +1,26 @@
+import "./home.scss";
+
+const Home = () => {
+  return (
+    <div className="content home">
+      <h3>Welcome To Website</h3>
+      <br />
+      <br />
+      <span>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur omnis
+        adipisci expedita tenetur alias vitae natus suscipit ab ipsam ad, culpa
+        modi itaque mollitia velit quibusdam, et quas fugit. Consequatur enim
+        provident distinctio deserunt, maiores ad magni laboriosam suscipit
+        dolorem? Facere magnam laborum quibusdam. Maiores minus, aut, facilis
+        culpa fugiat, vero quos sequi vel doloribus officia blanditiis nam earum
+        necessitatibus sapiente! Id necessitatibus enim praesentium possimus ut
+        ratione quod odit est, aut excepturi cumque voluptas voluptatum neque
+        quidem nostrum omnis eum, voluptate pariatur? Quo quis in incidunt. Quos
+        voluptate quisquam aperiam corrupti incidunt esse fugiat ratione,
+        architecto ea, quibusdam aliquam.
+      </span>
+    </div>
+  );
+};
+
+export default Home;
