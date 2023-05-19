25f12afe6d8d6c698bb90d3057ca7d5cf322731a
AP <aleksandrpolishchuk@gmail.com>
2023-05-19 23:55:29 +0700

    add Home to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 import { ThemeContext } from "./context/theme.context";
 import Navbar from "./components/navbar/Navbar.component";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home.page";
 
 const App = () => {
   const { darkMode } = useContext(ThemeContext);
@@ -11,7 +13,11 @@ const App = () => {
   return (
     <div className={appStyles}>
       <Navbar />
-      <div className="wrapper">Routes</div>
+      <div className="wrapper">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </div>
   );
 };
