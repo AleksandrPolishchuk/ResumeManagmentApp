7bed12ced004b1f26bcd4439de0cfa70c3be7df6
AP <aleksandrpolishchuk@gmail.com>
2023-05-20 23:42:30 +0700

    add fetch data for Companies.page

--- frontend/src/pages/companies/Companies.page.tsx
+++ frontend/src/pages/companies/Companies.page.tsx
@@ -1,8 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./companies.scss";
 import httpModule from "../../helpers/http.module";
+import { ICompany } from "../../types/global.typing";
 
 const Companies = () => {
+  const [companies, setCompanies] = useState<ICompany[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    setLoading(true);
+    httpModule
+      .get<ICompany[]>("/Company/Get")
+      .then((response) => {
+        setCompanies(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        alert("Error");
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+
+  console.log(companies);
+
   return <div>Companies.page</div>;
 };
 
