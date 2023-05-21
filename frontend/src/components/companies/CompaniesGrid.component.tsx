c6a98ca128aa6b78a7298999954be136e90d6e00
AP <aleksandrpolishchuk@gmail.com>
2023-05-21 23:16:18 +0700

    add moment

--- frontend/src/components/companies/CompaniesGrid.component.tsx
+++ frontend/src/components/companies/CompaniesGrid.component.tsx
@@ -1,7 +1,20 @@
+import { Box } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import React from "react";
 
-const CompaniesGrid = () => {
-  return <div className="companies-grid">CompaniesGrid.component</div>;
+const column: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 100 },
+  { field: "name", headerName: "Name", width: 200 },
+  { field: "size", headerName: "Size", width: 150 },
+  { field: "createdAt", headerName: "Creation Time", width: 200 },
+];
+
+const CompaniesGrid = ({ data }) => {
+  return (
+    <Box className="companies-grid">
+      <DataGrid rows={data} columns={} />
+    </Box>
+  );
 };
 
 export default CompaniesGrid;
