0edffa8315dd3893959a9524882b53ff2665431e
AP <aleksandrpolishchuk@gmail.com>
2023-05-22 00:00:17 +0700

    add changes in AddCompany.page

--- frontend/src/pages/companies/AddCompany.page.tsx
+++ frontend/src/pages/companies/AddCompany.page.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { useState } from "react";
+import { ICreateCompanyDto } from "../../types/global.typing";
+import { TextField } from "@mui/material";
 
 const AddCompany = () => {
-  return <div>AddCompany.page</div>;
+  const [company, setCompany] = useState<ICreateCompanyDto>({
+    name: "",
+    size: "",
+  });
+  return (
+    <div className="content">
+      <div className="add-company">
+        <h2>Add New Company</h2>
+        <TextField
+          autoComplete="off"
+          label="Company Name"
+          variant="outlined"
+          value={company.name}
+          onChange={e => setCompany(...company, name: e.target.value)}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default AddCompany;
