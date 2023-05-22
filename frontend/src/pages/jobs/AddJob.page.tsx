a266842c889b0c0c2dd70ddd4019e35a072799e2
AP <aleksandrpolishchuk@gmail.com>
2023-05-22 23:03:20 +0700

    add AddJob.page

---
+++ frontend/src/pages/jobs/AddJob.page.tsx
@@ -0,0 +1,126 @@
+import { useState, useEffect } from "react";
+import { ICompany, ICreateJobDto } from "../../types/global.typing";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import httpModule from "../../helpers/http.module";
+import "./companies.scss";
+
+const levelsArray: string[] = [
+  "Intern",
+  "Junior",
+  "MidLevel",
+  "Senior",
+  "TeamLead",
+  "Cto",
+  "Architect",
+];
+
+const AddJob = () => {
+  const [job, setJob] = useState<ICreateJobDto>({
+    title: "",
+    level: "",
+    companyId: "",
+  });
+  const [companies, setCompanies] = useState<ICompany[]>([]);
+
+  const redirect = useNavigate();
+
+  useEffect(() => {
+    httpModule
+      .get<ICompany[]>("/Company/Get")
+      .then((response) => {
+        setCompanies(response.data);
+      })
+      .catch((error) => {
+        alert("Error");
+        console.log(error);
+      });
+  }, []);
+
+  const handleClickSaveBtn = () => {
+    /*
+    if (company.name === "" || company.size === "") {
+      alert("Fill all fields");
+      return;
+    }
+    httpModule
+      .post("/Company/Create", company)
+      .then((response) => redirect("/companies"))
+      .catch((error) => console.log(error));
+      */
+  };
+
+  const handleClickBackBtn = () => {
+    redirect("/companies");
+  };
+
+  return (
+    <div className="content">
+      <div className="add-job">
+        <h2>Add New Job</h2>
+        <TextField
+          autoComplete="off"
+          label="Job Title"
+          variant="outlined"
+          value={job.title}
+          onChange={(e) => setJob({ ...job, title: e.target.value })}
+        />
+        <FormControl fullWidth>
+          <InputLabel>Job Level</InputLabel>
+          <Select
+            value={job.level}
+            label="Job Level"
+            onChange={(e) => setJob({ ...job, level: e.target.value })}
+          >
+            {levelsArray.map((item) => (
+              <MenuItem key={item} value={item}>
+                {item}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        <FormControl fullWidth>
+          <InputLabel>Company</InputLabel>
+          <Select
+            value={job.companyId}
+            label="Company"
+            onChange={(e) => setJob({ ...job, companyId: e.target.value })}
+          >
+            {companies.map((item) => (
+              <MenuItem key={item.id} value={item.id}>
+                {item.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        <div className="btns">
+          <Button
+            variant="outlined"
+            color="primary"
+            onClick={handleClickSaveBtn}
+          >
+            Save
+          </Button>
+          <Button
+            variant="outlined"
+            color="secondary"
+            onClick={handleClickBackBtn}
+          >
+            Back
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddJob;
