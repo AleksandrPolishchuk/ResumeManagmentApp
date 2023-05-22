da6749e706567d1e536b70fa28852b3ce63165cc
AP <aleksandrpolishchuk@gmail.com>
2023-05-22 23:35:37 +0700

    add ICandidate & ICreateCandidateDto

--- frontend/src/types/global.typing.ts
+++ frontend/src/types/global.typing.ts
@@ -24,3 +24,24 @@ export interface ICreateJobDto {
   level: string;
   companyId: string;
 }
+
+export interface ICandidate {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  coverLetter: string;
+  resumeUrl: string;
+  jobId: string;
+  jobTitle: string;
+}
+
+export interface ICreateCandidateDto {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  coverLetter: string;
+  jobId: string;
+}
