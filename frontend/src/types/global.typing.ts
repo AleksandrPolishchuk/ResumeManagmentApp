faeee3c1e4dabb7d13b24d25ebc9d279a031816d
AP <aleksandrpolishchuk@gmail.com>
2023-05-22 21:47:02 +0700

    add IJob & ICreateJobDto

--- frontend/src/types/global.typing.ts
+++ frontend/src/types/global.typing.ts
@@ -9,3 +9,18 @@ export interface ICreateCompanyDto {
   name: string;
   size: string;
 }
+
+export interface IJob {
+  id: string;
+  title: string;
+  level: string;
+  companyId: string;
+  companyName: string;
+  createdAt: string;
+}
+
+export interface ICreateJobDto {
+  title: string;
+  level: string;
+  companyId: string;
+}
