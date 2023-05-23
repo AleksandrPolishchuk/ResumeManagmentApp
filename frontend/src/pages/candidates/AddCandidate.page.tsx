10391a9c0ea6fc7fbe1f7d4b9a259951f5e43510
AP <aleksandrpolishchuk@gmail.com>
2023-05-23 20:55:11 +0700

    add AddCandidate.page

---
+++ frontend/src/pages/candidates/AddCandidate.page.tsx
@@ -0,0 +1,126 @@
+import { useState, useEffect } from "react";
+import { ICompany, ICreateCandidateDto, IJob } from "../../types/global.typing";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import httpModule from "../../helpers/http.module";
+import "./candidates.scss";
+
+const AddCandidate = () => {
+  const [candidate, setCandidate] = useState<ICreateCandidateDto>({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    coverLetter: "",
+    jobId: "",
+  });
+  const [jobs, setJobs] = useState<IJob[]>([]);
+  const [pdfFile, setPdfFile] = useState<File | null>();
+
+  const redirect = useNavigate();
+
+  useEffect(() => {
+    httpModule
+      .get<IJob[]>("/Job/Get")
+      .then((response) => {
+        setJobs(response.data);
+      })
+      .catch((error) => {
+        alert("Error");
+        console.log(error);
+      });
+  }, []);
+  /*
+  const handleClickSaveBtn = () => {
+    if (job.title === "" || job.level === "" || job.companyId === "") {
+      alert("Fill all fields");
+      return;
+    }
+    httpModule
+      .post("/Job/Create", job)
+      .then((response) => redirect("/jobs"))
+      .catch((error) => console.log(error));
+  };*/
+
+  const handleClickBackBtn = () => {
+    redirect("/candidates");
+  };
+
+  return (
+    <div className="content">
+      <div className="add-candidate">
+        <h2>Add New Candidate</h2>
+        <FormControl fullWidth>
+          <InputLabel>Job</InputLabel>
+          <Select
+            value={candidate.jobId}
+            label="Job"
+            onChange={(e) =>
+              setCandidate({ ...candidate, jobId: e.target.value })
+            }
+          >
+            {jobs.map((item) => (
+              <MenuItem key={item.id} value={item.id}>
+                {item.title}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <TextField
+          autoComplete="off"
+          label="First Name"
+          variant="outlined"
+          value={candidate.firstName}
+          onChange={(e) =>
+            setCandidate({ ...candidate, firstName: e.target.value })
+          }
+        />
+        <TextField
+          autoComplete="off"
+          label="Last Name"
+          variant="outlined"
+          value={candidate.lastName}
+          onChange={(e) =>
+            setCandidate({ ...candidate, lastName: e.target.value })
+          }
+        />
+        <TextField
+          autoComplete="off"
+          label="Email"
+          variant="outlined"
+          value={candidate.email}
+          onChange={(e) =>
+            setCandidate({ ...candidate, email: e.target.value })
+          }
+        />
+        <TextField
+          autoComplete="off"
+          label="Phone"
+          variant="outlined"
+          value={candidate.phone}
+          onChange={(e) =>
+            setCandidate({ ...candidate, phone: e.target.value })
+          }
+        />
+        <TextField
+          autoComplete="off"
+          label="C V"
+          variant="outlined"
+          value={candidate.coverLetter}
+          onChange={(e) =>
+            setCandidate({ ...candidate, coverLetter: e.target.value })
+          }
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AddCandidate;
