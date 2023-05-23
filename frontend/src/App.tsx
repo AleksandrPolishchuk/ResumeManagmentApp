02d37dd841557982caeef363273a8f57adc974ef
AP <aleksandrpolishchuk@gmail.com>
2023-05-23 21:18:37 +0700

    add route for AddCandidate.page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,7 @@ const AddCompany = lazy(() => import("./pages/companies/AddCompany.page"));
 const Jobs = lazy(() => import("./pages/jobs/Jobs.page"));
 const AddJob = lazy(() => import("./pages/jobs/AddJob.page"));
 const Candidates = lazy(() => import("./pages/candidates/Candidates.page"));
+const AddCandidate = lazy(() => import("./pages/candidates/AddCandidate.page"));
 
 const App = () => {
   const { darkMode } = useContext(ThemeContext);
@@ -35,7 +36,7 @@ const App = () => {
             </Route>
             <Route path="/candidates">
               <Route index element={<Candidates />} />
-              {/*<Route path="add" element={<AddJob />} /> */}
+              <Route path="add" element={<AddCandidate />} />
             </Route>
           </Routes>
         </Suspense>
